9b2853ef7cd2a48f5349f2f03b541bfe455eb285
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-05-01 17:03:12 -0300

    Reset

---
+++ A Game A Week/Assets/Scripts/Player Controls/Player_2D.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_2D : MonoBehaviour
+{
+    public float speed;
+
+    public KeyCode right;
+    public KeyCode rightAlt;
+    public KeyCode left;
+    public KeyCode leftAlt;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(right) || Input.GetKey(rightAlt))
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        if(Input.GetKey(left)|| Input.GetKey(leftAlt))
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+    }
+
+    public void BounceForce(Vector3 force)
+    {
+        GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+    }
+}
---
+++ A Game A Week/Assets/Scripts/Player Controls/Player_2D_Jump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_2D_Jump : MonoBehaviour
+{
+    public float jumpForce;
+    public float groundRange;
+
+    public Transform groundCheck;
+    public LayerMask ground_Mask;
+
+    public KeyCode jump;
+    public KeyCode jumpAlt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(jump) || Input.GetKeyDown(jumpAlt))
+        {
+            if (Physics.CheckSphere(groundCheck.position, groundRange, ground_Mask)) 
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
+    }
+}
