dbe95d922cc7c70dcd0e6c820f49fe6b4e868922
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-06-13 23:50:59 -0300

    game 1 start

---
+++ A Game A Week/Assets/Scripts/Camera/FPS_Rotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_Rotation : MonoBehaviour
+{
+    public float speed, xMin, xMax;
+    public GameObject characterObject;
+
+    private float xRot = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xRotation = Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+        float yRotation = Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+
+         xRot -= yRotation;
+
+        xRot = Mathf.Clamp(xRot, xMin, xMax);
+
+        characterObject.transform.Rotate(0f, xRotation , 0f);
+        transform.localRotation = Quaternion.Euler(xRot , 0f, 0f);
+    }
+}
---
+++ A Game A Week/Assets/Scripts/Player Controls/FPS_Movement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_Movement : MonoBehaviour
+{
+    public float vertical_Speed;
+    public float horizontal_Speed;
+
+    public KeyCode _UpKey, _altUpKey;
+    public KeyCode _DownKey, _altDownKey;
+
+    public KeyCode _LeftKey, _altLeftKey;
+    public KeyCode _RightKey, _altRightKey;
+
+    float speed_Multiplier = 1;
+    public float speed_Sprint;
+    public KeyCode _Sprint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Speed();
+
+        if (Input.GetKey(_UpKey) || Input.GetKey(_altUpKey))
+        {
+            transform.Translate(0, 0, vertical_Speed * speed_Multiplier * Time.deltaTime);
+        }
+        else if (Input.GetKey(_DownKey) || Input.GetKey(_altDownKey))
+        {
+            transform.Translate(0, 0, -vertical_Speed * speed_Multiplier * Time.deltaTime);
+        }
+
+        if (Input.GetKey(_RightKey) || Input.GetKey(_altRightKey))
+        {
+            transform.Translate(horizontal_Speed * speed_Multiplier * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(_LeftKey) || Input.GetKey(_altLeftKey))
+        {
+            transform.Translate(-horizontal_Speed * speed_Multiplier * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void Speed()
+    {
+        if (Input.GetKey(_Sprint))
+        {
+            speed_Multiplier = speed_Sprint;
+        }
+        else
+        {
+            speed_Multiplier = 1;
+        }
+    }
+}
