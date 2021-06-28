8ae44850af39a04418dcfc4aa27e9baf08000677
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-06-28 15:18:52 -0300

    game 2 finished - game 3 start

---
+++ A Game A Week/Assets/Scripts/Level Mechanics/DoorMechanic.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorMechanic : MonoBehaviour
+{
+    public Text                     propmt;
+    public float                    openAmount;
+    public float                    lerpTime;
+    public float                    timer;
+    //public GameObject               doorLeft, doorRight;
+
+    private bool                    opened;
+    private PlayerInteractions1     playerInput;
+    private float                   doorThreshold;
+    private Vector3                 resetPos, newPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorThreshold = 0f;
+        resetPos = gameObject.transform.rotation.eulerAngles;
+        newPos = resetPos;
+        newPos.y += openAmount; 
+        playerInput = null;
+        opened = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInput.PlayerInteract())
+        {
+            StartCoroutine(OpenDoor());
+        }
+    }
+
+    IEnumerator OpenDoor()
+    {
+        yield return null;
+        
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(newPos), timer);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInput = other.GetComponent<PlayerInteractions1>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerInput = null;
+    }
+
+}
---
+++ A Game A Week/Assets/Scripts/Player/First Person Shooter/PlayerInteractions1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions1 : MonoBehaviour
+{
+    public KeyCode      interactKey;
+    private bool        interaction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(interactKey)) { interaction = true; }
+        else interaction = false;
+    }
+
+    public bool PlayerInteract() => interaction;
+
+}
